80a83d9eafb9b1c6dda36198770395751f23a49b
tnrdd <tnrdd@tutanota.com>
2025-04-16 10:57:03 +0200

    feat: add council managers to subgraph

--- contracts/councilhaus/subgraph/src/council-factory.ts
+++ contracts/councilhaus/subgraph/src/council-factory.ts
@@ -1,3 +1,4 @@
+import { ethereum, crypto, Bytes } from "@graphprotocol/graph-ts";
 import { CouncilCreated as CouncilCreatedEvent } from "../generated/CouncilFactory/CouncilFactory";
 import { Council } from "../generated/schema";
 import { Council as CouncilTemplate } from "../generated/templates";
@@ -6,10 +7,19 @@ import { Council as CouncilContract } from "../generated/templates/Council/Counc
 export function handleCouncilCreated(event: CouncilCreatedEvent): void {
   const councilAddress = event.params.council;
   const entity = new Council(event.params.council.toHex());
+  const memberManagerRole = Bytes.fromByteArray(
+    crypto.keccak256(Bytes.fromUTF8("MEMBER_MANAGER_ROLE")),
+  );
+  const granteeManagerRole = Bytes.fromByteArray(
+    crypto.keccak256(Bytes.fromUTF8("GRANTEE_MANAGER_ROLE")),
+  );
 
   const councilContract = CouncilContract.bind(councilAddress);
 
   entity.metadata = event.params.metadata;
+  entity.pool = event.params.pool;
+  entity.memberManagerRole = memberManagerRole;
+  entity.granteeManagerRole = granteeManagerRole;
   entity.pool = event.params.pool;
   entity.distributionToken = councilContract.distributionToken();
   entity.maxAllocationsPerMember = councilContract.maxAllocationsPerMember();
--- contracts/councilhaus/subgraph/src/council.ts
+++ contracts/councilhaus/subgraph/src/council.ts
@@ -5,8 +5,11 @@ import {
   Council,
   CouncilMember,
   Grantee,
+  CouncilManager,
 } from "../generated/schema";
 import {
+  RoleGranted,
+  RoleRevoked,
   BudgetAllocated,
   CouncilMemberAdded,
   CouncilMemberRemoved,
@@ -15,88 +18,116 @@ import {
   MaxAllocationsPerMemberSet,
 } from "../generated/templates/Council/Council";
 
-// Handle council member added
+export function handleRoleGranted(event: RoleGranted): void {
+  const councilManager = new CouncilManager(
+    `${event.params.role.toHex()}-${event.params.account.toHex()}`,
+  );
+  const council = Council.load(event.address.toHex());
+
+  if (!council) {
+    log.warning("Council not found for role {} and account {}", [
+      event.params.role.toHex(),
+      event.params.account.toHex(),
+    ]);
+
+    return;
+  }
+
+  councilManager.account = event.params.account;
+  councilManager.role = event.params.role;
+  councilManager.council = council.id;
+  councilManager.createdAt = event.block.number;
+
+  councilManager.save();
+}
+
+export function handleRoleRevoked(event: RoleRevoked): void {
+  const id = `${event.params.role.toHex()}-${event.params.account.toHex()}`;
+
+  store.remove("CouncilManager", id);
+}
+
 export function handleCouncilMemberAdded(event: CouncilMemberAdded): void {
   const councilMember = new CouncilMember(
     `${event.address.toHex()}-${event.params.member.toHex()}`,
   );
   councilMember.account = event.params.member;
   councilMember.votingPower = event.params.votingPower;
-  councilMember.council = event.address.toHex(); // Linking to the council
+  councilMember.council = event.address.toHex();
 
   councilMember.save();
 }
 
-// Handle council member removed
 export function handleCouncilMemberRemoved(event: CouncilMemberRemoved): void {
   const councilMemberId = `${event.address.toHex()}-${event.params.member.toHex()}`;
 
   store.remove("CouncilMember", councilMemberId);
 }
 
-// Handle grantee added
 export function handleGranteeAdded(event: GranteeAdded): void {
   const grantee = new Grantee(
     `${event.address.toHex()}-${event.params.grantee.toHex()}`,
   );
   grantee.metadata = event.params.metadata;
   grantee.account = event.params.grantee;
-  grantee.council = event.address.toHex(); // Linking to the council
+  grantee.council = event.address.toHex();
 
   grantee.save();
 }
 
-// Handle grantee removed
 export function handleGranteeRemoved(event: GranteeRemoved): void {
   const granteeId = `${event.address.toHex()}-${event.params.grantee.toHex()}`;
 
   store.remove("Grantee", granteeId);
 }
 
-// Handle budget allocated
 export function handleBudgetAllocated(event: BudgetAllocated): void {
   const allocation = new Allocation(
     `${event.transaction.hash.toHex()}-${event.logIndex.toString()}`,
   );
-
   const councilMember = CouncilMember.load(
     `${event.address.toHex()}-${event.params.member.toHex()}`,
   );
+
   if (!councilMember) {
     log.warning("Council member not found, skipping allocation", [
       event.params.member.toHex(),
     ]);
     return;
   }
+
   allocation.council = event.address.toHex();
   allocation.councilMember = councilMember.id;
   allocation.allocatedAt = event.block.timestamp;
   allocation.amounts = event.params.allocation.amounts;
 
-  // Check all grantees are valid
   const grantees: string[] = [];
   const accounts = event.params.allocation.accounts;
+
   for (let i = 0; i < accounts.length; i++) {
     const grantee = Grantee.load(
       `${event.address.toHex()}-${accounts[i].toHex()}`,
     );
+
     if (!grantee) {
       log.warning("Not all grantees found, skipping allocation", [
         accounts[i].toHex(),
       ]);
       return;
     }
+
     grantees.push(grantee.id);
   }
+
   allocation.grantees = grantees;
   allocation.save();
 }
 
-// Handle max allocations per member set
 export function handleMaxAllocationsPerMemberSet(
   event: MaxAllocationsPerMemberSet,
 ): void {
   const council = Council.load(event.address.toHex());
+
   if (council) {
     council.maxAllocationsPerMember = event.params.maxAllocationsPerMember;
     council.save();
